447c7d6c1d582f9dd416b5539312a9fec5b58fe8
Eric-Sprung <sprung@th-brandenburg.de>
2020-10-19 16:39:10 +0200

    first generated client from node to node

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-let fs = require('fs');
-let CodeGen = require('swagger-js-codegen').CodeGen;
-let file = 'swagger/metamodell.json';
-let swagger = JSON.parse(fs.readFileSync(file, 'UTF-8'));
-let nodejsSourceCode = CodeGen.getNodeCode({ className: 'Test', swagger: swagger });
-let angularjsSourceCode = CodeGen.getAngularCode({ className: 'Test', swagger: swagger });
-let reactjsSourceCode = CodeGen.getReactCode({ className: 'Test', swagger: swagger });
-let tsSourceCode = CodeGen.getTypescriptCode({ className: 'Test', swagger: swagger, imports: ['../../typings/tsd.d.ts'] });
-//console.log(nodejsSourceCode);
-//console.log(angularjsSourceCode);
-//console.log(reactjsSourceCode);
-//console.log(tsSourceCode);
-/*
-fs.writeFile("filename.js", nodejsSourceCode, "utf-8", (err)=>{
-    if(err){
-        console.error(err);
-    }else{
-        console.log("worked");
-    }
-});
-*/
+const path = require('path');
+const codegen = require('swagger-node-codegen');
 
-let generatedClient = new Function (nodejsSourceCode);
-let products = generatedClient.listProductsUsingGET()
-console.log(products);
+codegen.generate({
+    swagger: path.resolve(__dirname, './swagger/metamodell.json'),
+    target_dir: path.resolve(__dirname, './my-api-client')
+  }).then(() => {
+    console.log('Done!');
+  }).catch(err => {
+    console.error(`Something went wrong: ${err.message}`);
+  });
